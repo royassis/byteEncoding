838ec6630f33df6d6db8b83bc36b9a5362d3972b
Roy <royassisg@gmail.com>
2021-01-06 00:04:39 +0200

    Tidyup
    Add typing comments and reformatting

--- bin_encoding_to_csv.py
+++ bin_encoding_to_csv.py
@@ -1,17 +1,46 @@
+"""
+A client we are working with encoded data in bytes.
+
+Use this code to extract information about sensors measurements
+encoded in repeted sequences of 8 bytes.
+
+[timestamp [8bytes] , measurement [8bytes] ]*
+"""
+
 import datetime
 import array
+from typing import Generator, Union, Any
 
-def byte_to_double(new_bytearr):
+
+def byte_to_double(new_bytearr: bytearray) -> float:
+    """
+    Wrapper for array.array()
+
+    :param new_bytearr: 8 byte chunck representing Double
+    :return: float
+    """
     return array.array('d', new_bytearr)[0]
 
-def bytes_to_datetime(new_bytearr):
+
+def bytes_to_datetime(new_bytearr: bytearray) -> datetime:
+    """
+    Converts eight bytes to datetime
+
+    :param new_bytearr: 8 byte chunck representing Double
+    :return: datetime object
+    """
     doubles_sequence = byte_to_double(new_bytearr)
     seconds = (doubles_sequence - 25569) * 86400.0
     dt_obj = datetime.datetime.utcfromtimestamp(seconds)
 
     return dt_obj.strftime("%Y-%m-%d-%H-%M-%S")
 
-def read_bytes_generator(p):
+
+def read_bytes_generator(p: str) -> Generator[list[Union[str, float]], Any, None]:
+    """
+    :param p: string representing filepath
+    :return: generator
+    """
     double_bytes = 8
 
     with open(p, "rb") as fp:
@@ -25,10 +54,16 @@ def read_bytes_generator(p):
             dt = bytes_to_datetime(datetime_bytes)
             measure = byte_to_double(measure_bytes)
 
-            yield([dt, measure])
+            yield ([dt, measure])
 
-p = "40bytes_from_original.txt"
-bytesgen = read_bytes_generator(p)
 
-for i in bytesgen:
-    print(i)
+def main():
+    p = "40bytes_from_original.txt"
+    bytesgen = read_bytes_generator(p)
+
+    for i in bytesgen:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
