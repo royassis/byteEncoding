cf695e8def8384c066f8804f075fa83a8dfe182a
Roy <royassisg@gmail.com>
2021-01-18 09:31:56 +0200

    Try And Test

--- byte_encoding/FileOrEquivalentIterators.py
+++ byte_encoding/FileOrEquivalentIterators.py
@@ -5,24 +5,23 @@ from utils import decode_sgement
 
 
 class BaseFileIterator(ABC):
-    def __init__(self, data):
-        self.data = data
-        self.__file_object = None
+    def __init__(self):
+        self.file_object = None
 
     def __enter__(self):
-        self.__file_object = io.BytesIO(self.data)
+        self.file_object = open(self._path, "rb")
         return self
 
     def __exit__(self, type, val, tb):
-        self.__file_object.close()
+        self.file_object.close()
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        segment_bytes = self.virtual_file_hadnle.read(SEGMENT_SIZE)
+        segment_bytes = self.file_object.read(SEGMENT_SIZE)
 
-        if self.__file_object is None or segment_bytes == b'':
+        if self.file_object is None or segment_bytes == b'':
             raise StopIteration
         else:
             timestamp, value = decode_sgement(segment_bytes)
@@ -33,19 +32,32 @@ class BaseFileIterator(ABC):
 class BytesArrayIterator(BaseFileIterator):
     def __init__(self, data):
         self.data = data
-        self.__file_object = None
+        self.file_object = io.BytesIO(data)
 
-    def __enter__(self):
-        self.__file_object = io.BytesIO(self.data)
+    def __iter__(self):
         return self
+
+    def __next__(self):
+        segment_bytes = self.file_object.read(SEGMENT_SIZE)
+
+        if self.file_object is None or segment_bytes == b'':
+            raise StopIteration
+        else:
+            timestamp, value = decode_sgement(segment_bytes)
+
+            return timestamp, value
+
 
 
 class FileIterator(BaseFileIterator):
 
     def __init__(self, file_path):
         self._path = file_path
-        self.__file_object = None
+        self.file_object = None
 
     def __enter__(self):
-        self.__file_object = open(self._path, "rb")
+        self.file_object = open(self._path, "rb")
+        return self
+
+    def __iter__(self):
         return self
---
+++ byte_encoding/tests/integration/test_file_iterator.py
@@ -0,0 +1,26 @@
+import unittest
+from FileOrEquivalentIterators import FileIterator, BytesArrayIterator
+from pathlib import Path
+
+path = Path(r"C:\Users\Roy\PycharmProjects\byte_encoding\byte_encoding\tests\fixtures\Pen2_20170909_raw")
+zpath = Path(r"C:\Users\Roy\PycharmProjects\byte_encoding\byte_encoding\tests\fixtures\wserver102-Week-53-Year-2020.zip")
+
+with open(path, "rb") as f:
+    data = f.read()
+
+class TestFileIterator(unittest.TestCase):
+
+    def test_sain_FileIterator(self):
+        with FileIterator(path) as f:
+            for _ in f:
+                break
+
+    def test_sain_BytesArrayIterator(self):
+
+        with BytesArrayIterator(data) as f:
+            for _ in f:
+                break
+
+
+if __name__ == '__main__':
+    unittest.main()
