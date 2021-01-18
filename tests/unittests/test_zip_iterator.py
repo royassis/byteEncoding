d5627d6dda33ee3687d89042ee2859525477c209
Roy <royassisg@gmail.com>
2021-01-18 09:40:02 +0200

    Change proj struct

--- tests/unittests/test_zip_iterator.py
+++ tests/unittests/test_zip_iterator.py
@@ -2,20 +2,20 @@ import unittest
 from ZipIterators import ZipFileExtractorIterator, ZipFileHandleIterator
 from pathlib import Path
 
-path = Path(r"/tests/fixtures/Pen2_20170909_raw")
-zpath = Path(r"/tests/fixtures/wserver102-Week-53-Year-2020.zip")
+path = Path(r"../fixtures/Pen2_20170909_raw")
+zpath = Path(r"../fixtures/wserver102-Week-53-Year-2020.zip")
 
 class TestZipFileExtractorIterator(unittest.TestCase):
 
     def test_sain_ZipFileExtractorIterator(self):
         with ZipFileExtractorIterator(zpath) as ziter:
             for stuff in ziter:
-                print(stuff)
+                break
 
     def test_sain_ZipFileHandleIterator(self):
         with ZipFileHandleIterator(zpath) as ziter:
             for stuff in ziter:
-                print(stuff)
+                break
 
 if __name__ == '__main__':
     unittest.main()
